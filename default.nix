0a97df9746c625dbeff5b04e41c95c1fc2848ef6
willjr <willjr@koan.co.nz>
2023-09-20 09:42:24 +1200

    Add JDK 19 to "all packages"

--- default.nix
+++ default.nix
@@ -1,21 +1,21 @@
-{ pkgs,
-  callPackage,
+{ callPackage,
   buildEnv,
 }:
 
 let
   packages = callPackage ./packages { };
-  inherit (packages) aws-cloud-tools git node python-with-packages serverless terraform;
+  inherit (packages) aws-cloud-tools git java node python-with-packages serverless terraform;
 
 in buildEnv {
   # pathsToLink ignoreCollisions postBuild;
   extraOutputsToInstall = [];
 
   name = "all-packages";
-  paths = with pkgs; [
+  paths = [
     aws-cloud-tools.cdk
     aws-cloud-tools.cli
     git.all
+    java.v19
     node.v18
     python-with-packages
     terraform.latest
