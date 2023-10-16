ff736711dd71fab793eb67aa179da1f74d5d6b6b
willjr <willjr@koan.co.nz>
2023-10-16 20:07:29 +1300

    Add Java JDK 11

--- packages/java/default.nix
+++ packages/java/default.nix
@@ -1,14 +1,16 @@
 { openjdk8,
+  openjdk11,
   openjdk17,
   openjdk19,
 }:
 
 let
   jdk-8 = openjdk8;
+  jdk-11 = openjdk11;
   jdk-17 = openjdk17;  # Current LTS
   jdk-19 = openjdk19;
 
 in {
-  inherit jdk-8 jdk-17 jdk-19;
+  inherit jdk-8 jdk-11 jdk-17 jdk-19;
   jdk = jdk-19;
 }
