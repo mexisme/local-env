7486711a03549c40b0128fbe1f58d2d771d7ab33
willjr <willjr@koan.co.nz>
2023-11-28 17:35:52 +1300

    Update NPM deps for NestJS

--- packages/nestjs/default.nix
+++ packages/nestjs/default.nix
@@ -15,7 +15,7 @@ in buildNpmPackage {
   # If the "package-lock.json" needs updating, NPM Deps Hash can be calculated by running the following in this dir:
   #   nix run 'nixpkgs#prefetch-npm-deps' -- ./package-lock.json
   # ... and replacing the below SHA hash with the value output:
-  npmDepsHash = "sha256-vZ6H1qgYocFJgn+L1r/GJ8nRmBIw5JMPeWSj8mw8KZc=";
+  npmDepsHash = "sha256-ocqRmMIZ3eumwQFnVpxr1F+s41KDWBl4Ap1q+DoeZHE=";
 
   dontNpmBuild = true;
   npmInstallFlags = [ "--bin-links" ];
