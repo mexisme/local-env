5dff61856fc559cd48379b5a4c9ccae601f858b6
willjr <willjr@koan.co.nz>
2023-12-22 16:43:06 +1300

    Create w "with-..." wrapper to make it possible to run scripts under JDK-19

--- packages/java/default.nix
+++ packages/java/default.nix
@@ -2,6 +2,7 @@
   openjdk11,
   openjdk17,
   openjdk19,
+  writeShellApplication,
 }:
 
 let
@@ -10,7 +11,28 @@ let
   jdk-17 = openjdk17;  # Current LTS
   jdk-19 = openjdk19;
 
+  java-wrapper =
+    {
+      name,
+      package ? jdk-19,
+      runtimeInputs ? [],
+    }: writeShellApplication {
+      name = "with-${name}";
+      runtimeInputs = [ package ] ++ runtimeInputs;
+      text = ''
+        main() {
+          : "''${1:?"#ERR: Binary to wrap not provided?"}"
+
+          export JAVA_HOME="${package}/lib/openjdk"
+          exec "''$@"
+        }
+
+        main "''$@"
+    '';
+    };
+
 in {
   inherit jdk-8 jdk-11 jdk-17 jdk-19;
+  with-jdk-19 = java-wrapper { name = "jdk-19"; package = jdk-19; };
   jdk = jdk-19;
 }
